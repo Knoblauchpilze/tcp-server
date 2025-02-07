a40f80e08e6fac15a5f08d8bc7cc884dcbfcc282
KnoblauchPilze <prolific-programmer@tuta.com>
2025-02-07 11:36:19 +0100

    fix: Use environment variable instead of arg, fixed LD_LIBRARY_PATH and use ENTRYPOINT in docker file

--- data/debug.sh
+++ data/debug.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-CURR_DIR=$(dirname $0)
-
-export LD_LIBRARY_PATH="${CURR_DIR}/lib:${LD_LIBRARY_PATH}"
-
 APP_NAME=$1
-shift
 
-gdb --args ./bin/${APP_NAME} "$@"
+if [ "$#" -ge 2 ]; then
+  PORT=$2
+  export PORT=$PORT
+fi
+
+gdb --args ./bin/${APP_NAME}
