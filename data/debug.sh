1c3a038696626e5ca7df8480854cbdd0df18b052
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-02-07 09:59:43 +0100

    feat: Used ubuntu instead of alpine and made the server run

--- data/debug.sh
+++ data/debug.sh
@@ -3,4 +3,6 @@ APP_NAME=$1
 
 CURR_DIR=$(dirname $0)
 
+export LD_LIBRARY_PATH="${CURR_DIR}/lib:${LD_LIBRARY_PATH}"
+
 gdb --args ./bin/${APP_NAME}
--- data/run.sh
+++ data/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 APP_NAME=$1
 
-export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
-
 CURR_DIR=$(dirname $0)
+
+export LD_LIBRARY_PATH="${CURR_DIR}/lib:${LD_LIBRARY_PATH}"
+
 ./bin/${APP_NAME}
