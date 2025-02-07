a3da4ba0724eebecdf30b865c985962de4cb2176
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-02-07 11:36:19 +0100

    fix: Use environment variable instead of arg, fixed LD_LIBRARY_PATH and use ENTRYPOINT in docker file

--- data/run.sh
+++ data/run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-CURR_DIR=$(dirname $0)
-
-export LD_LIBRARY_PATH="${CURR_DIR}/lib:${LD_LIBRARY_PATH}"
-
-# https://stackoverflow.com/questions/9057387/process-all-arguments-except-the-first-one-in-a-bash-script
 APP_NAME=$1
-shift
 
-./bin/${APP_NAME} "$@"
+if [ "$#" -ge 2 ]; then
+  PORT=$2
+  export PORT=$PORT
+fi
+
+./bin/${APP_NAME}
--- src/server/main.cc
+++ src/server/main.cc
@@ -15,35 +15,38 @@ void sigIntInterceptor(const int signal)
   sigIntProcessing(signal);
 }
 
-auto getPortFromArgs(const int argc, char *argv[]) -> int
+auto getPortFromEnvironmentVariable() -> int
 {
-  if (argc < 2)
+  constexpr auto SERVER_PORT_ENV_VAR_NAME = "PORT";
+  const auto maybeEnvVar                  = std::getenv(SERVER_PORT_ENV_VAR_NAME);
+
+  if (maybeEnvVar == nullptr)
   {
-    throw std::out_of_range("No port passed as arguments to the server");
+    throw std::out_of_range("No port passed as environment variable to the server");
   }
 
   int port = 0;
   try
   {
-    port = std::stoi(argv[1]);
+    port = std::stoi(maybeEnvVar);
   }
   catch (const std::invalid_argument &e)
   {
     std::string message("Failed to convert provided port: ");
-    message += argv[1];
+    message += maybeEnvVar;
     throw std::invalid_argument(message);
   }
   catch (const std::out_of_range &e)
   {
     std::string message("Provided port is not valid: ");
-    message += argv[1];
+    message += maybeEnvVar;
     throw std::invalid_argument(message);
   }
 
   if (port < 1 || port > 65535)
   {
     std::string message("Port should be in range ]1; 65536[, provided value is not: ");
-    message += argv[1];
+    message += maybeEnvVar;
     throw std::out_of_range(message);
   }
 
@@ -51,7 +54,7 @@ auto getPortFromArgs(const int argc, char *argv[]) -> int
 }
 } // namespace
 
-int main(int argc, char *argv[])
+int main(int /*argc*/, char * /*argv*/[])
 {
   core::log::StdLogger raw;
   raw.setLevel(core::log::Severity::DEBUG);
@@ -65,7 +68,7 @@ int main(int argc, char *argv[])
     // https://en.cppreference.com/w/cpp/utility/program/signal
     std::signal(SIGINT, sigIntInterceptor);
 
-    server.run(getPortFromArgs(argc, argv));
+    server.run(getPortFromEnvironmentVariable());
   }
   catch (const core::CoreException &e)
   {
